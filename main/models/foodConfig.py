a95acbd4b40913da33c06eaf1c0867e02ce8136c
三輪一晶 <>
2019-09-18 15:14:42 +0900

    update db

--- main/models/foodConfig.py
+++ main/models/foodConfig.py
@@ -4,5 +4,6 @@ from .user import User
 
 class FoodConfigParam(models.Model):
     name = models.CharField(max_length=100)
+    food_type = models.CharField(max_length=10)
     percent = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
