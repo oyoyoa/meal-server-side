9c7a237c5474c0aec7bd0db82d2d6d814563dbb8
kwbmnkkkk <be3737km@google.com>
2019-09-29 19:40:20 +0900

    random実装

--- main/views/meal_random.py
+++ main/views/meal_random.py
@@ -10,10 +10,40 @@ class RandomMeal(APIView):
     def get(self, request, format=None):
         foodConfigParams = FoodConfigParam.objects.filter(user=request.user)
         serializer = FoodConfigParamSerializer(foodConfigParams, many=True)
-        foods = []
-        for i in range(len(serializer.data)):
-            if serializer.data[i]['rate'] == 0:
-                continue
-            foods.append(serializer.data[i]['name'])
-        food = random.choice(foods)
-        return Response(food)
+        foods = serializer.data
+
+        count =0
+        vegi_list=[]
+        main_list=[]
+        resultMeal=[]
+        vegimax =0
+        mainmax =0
+
+        #野菜用の配列、肉魚用の配列作る
+        for food in foods:
+            if(food['food_type'] == 'vegetable'):
+                vegi_list.append(food)
+                vegimax +=int(food['rate'])
+                
+            else:
+                main_list.append(food)
+                mainmax +=int(food['rate'])
+                
+        randomNum =random.randrange((vegimax))
+        for vegi in vegi_list:
+            
+            count +=vegi['rate']
+            
+            if randomNum < count:
+                resultMeal.append(vegi['name'])
+                break
+
+        randomNum =random.randrange((mainmax))
+        for main in main_list:
+            
+            count +=main['rate']
+            
+            if randomNum < count:
+                resultMeal.append(main['name'])
+                break
+        return Response(resultMeal)
