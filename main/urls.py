690006bbefb2afa97abf5542853bb522a330a512
やよ <omim4841@gmail.com>
2019-10-16 19:29:14 +0900

    userごとに食材を取ってくるapiのurlを変更

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('signin/', views.AuthView.as_view()),
     path('randomMeal/', views.RandomMeal.as_view()),
     path('randomMeal/config/', views.RandomMealConfigList.as_view()),
-    path('randomMeal/user/configDetail/', views.RandomMealConfigDetail.as_view()),
+    path('randomMeal/user/config/', views.RandomMealConfigDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
