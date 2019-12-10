bc0a3548e149412aec1846ee7b228a2f66c0b32a
やよ <omim4841@gmail.com>
2019-12-10 23:23:31 +0900

    edit settings

--- config/settings/development.py
+++ config/settings/development.py
@@ -2,7 +2,7 @@ import os
 
 from .base import *
 
-SECRET_KEY = '76)5(c(fx4ton1h6ski0r1t3y9f$!q#q68+-drj3m&tmb9o8u_'
+SECRET_KEY = 'x3att3bk_55o5hpi01th_=g59t3w$x!+fcs5c7@5t)x5osqq)$'
 DEBUG = True
 JWT_AUTH['JWT_SECRET_KEY'] = SECRET_KEY
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', os.environ.get('ADDITIONAL_HOST')]
